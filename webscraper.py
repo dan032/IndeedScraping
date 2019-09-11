5cb565b96c7689c7c6784dbc5a7891115ff8c7c8
dan032 <buttsda@sheridancollege.ca>
2019-09-10 21:52:33 -0400

    Implement function to populate the jobs dictionary

--- webscraper.py
+++ webscraper.py
@@ -6,6 +6,7 @@ class Webscraper():
 
     def __init__(self):
         self._headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.3"}
+        self._jobDict = {}
 
 
     def get_jobs(self):
@@ -29,6 +30,24 @@ class Webscraper():
         location = input("\nWhere do you live: ")
         url = f'https://www.indeed.ca/jobs?q={search_term}&l={location}'
         return url
+    
+    def populate_jobs_dictionary(self, jobs):
+        """
+        Populates the jobDict dictionary, where the
+        keys are ascending ID's made locally, and
+        the values are a tuple containing the job title,
+        company, description and URL for the ad
+        """
+        job_id = 0
+        for job in jobs:
+            job_id += 1
+
+            title = job.find("div", class_="title").find("a", class_="jobtitle").text.strip()
+            title_url = job.find("div", class_="title").find("a", class_="jobtitle")['href']
+            job_url = f'indeed.ca{title_url}'
+            company = job.find("div", class_="sjcl").find("span").text.strip()
+            description = job.find("div", class_="summary").text.strip()
+            self._jobDict[job_id] = (title, company, description, job_url)
 
     def send_jobs_email(self):
         """
