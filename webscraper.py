b8e94b0e4c25a9b6fce884ed36aead1ceba1bcfd
dan032 <buttsda@sheridancollege.ca>
2019-09-10 21:44:55 -0400

    Created skeleton methods for the webscraper class

--- webscraper.py
+++ webscraper.py
@@ -2,3 +2,26 @@ class Webscraper():
 
     def __init__(self):
         pass
+
+    def get_jobs(self):
+        """
+        Sends an HTTP Get Request to specified URL
+        and returns all jobs listed on webpage
+        """
+        pass
+
+    def create_url(self):
+        """
+        Asks the user for their search terms and location and concatanates
+        them into the proper URL string for indeed.ca
+        """
+        pass
+
+    def send_jobs_email(self):
+        """
+        Sets up the SMTP connection to allow an email
+        to be sent, sends the email, and terminates the connection
+        """
+        pass
+
+  
