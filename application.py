29745b9b114f1e9f2eeaa8a184e72dcaa9c09984
dan032 <buttsda@sheridancollege.ca>
2019-09-10 21:37:57 -0400

    Initialized classes for application

---
+++ application.py
@@ -0,0 +1,9 @@
+from webscraper import Webscraper
+
+class Application:
+
+    def __init__(self):
+        self._webscraper = Webscraper()
+    
+    def main(self):
+        pass
---
+++ webscraper.py
@@ -0,0 +1,4 @@
+class Webscraper():
+
+    def __init__(self):
+        pass
